0f99308a0c9431f21c0f8f613e460bf81d651320
jonasek369 <69902785+jonasek369@users.noreply.github.com>
2022-04-11 17:12:36 +0200

    Update main.py

--- priklad 5/main.py
+++ priklad 5/main.py
@@ -25,24 +25,3 @@ def prvocislo():
         print("Toto není číslo")
 
 prvocislo()
-
-
-def prvocislo():
-    if cislo <= 0 or cislo == 1:
-        return False
-    if not cislo % 1 == 0:
-        print(f"{cislo} není prvočíslo")
-        exit()
-    if not cislo % cislo == 0:
-        print(f"{cislo} není prvočíslo")
-        exit()
-
-    # zkontroluje čislo jestli je dělitelné s čislem  1 - 9 pomocí % (modularu) ktery vrati zbytkovou
-    # hodnotu po dělení když to je 0 tak vím že čislo je tim to dělitelem dělitelné
-    for delitel in range(1, 10):
-        if delitel == 1 or delitel == cislo:
-            continue
-        if cislo % delitel == 0:
-            print(f"{cislo} není prvočíslo")
-            exit()
-    print(f"{cislo} je prvočíslo")
